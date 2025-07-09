5ce3e8427e6bf5be2faad6652e1b5f0929413f3b
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-07-09 19:01:55 +0300

    move ObjectContainer to file class PrototypeContainer

--- Scripts/UIKit/Managers/PrototypeManager.py
+++ Scripts/UIKit/Managers/PrototypeManager.py
@@ -1,6 +1,7 @@
 from Foundation.GroupManager import GroupManager
 from Foundation.DatabaseManager import DatabaseManager
 from UIKit.Managers.IconManager import IconManager
+from UIKit.Managers.PrototypeContainer import PrototypeContainer
 
 
 class PrototypeManager(object):
@@ -98,67 +99,5 @@ class PrototypeManager(object):
                 slot = object_unique.getMovieSlot(param_slot)
                 slot.addChild(icon_node)
 
-        container = ObjectContainer(object_unique, icon)
-
+        container = PrototypeContainer(object_unique, icon)
         return container
-
-
-class ObjectContainer(object):
-    def __init__(self, movie, icon=None):
-        self.movie = movie
-        self.icon = icon
-
-    def setEnable(self, state):
-        self.movie.setEnable(state)
-        if self.icon is not None:
-            self.icon.setEnable(state)
-
-    def onDestroy(self):
-        if self.movie is not None:
-            self.movie.onDestroy()
-            self.movie = None
-        if self.icon is not None:
-            self.icon.onDestroy()
-            self.icon = None
-
-    def setTextAliasEnvironment(self, text_env):
-        self.movie.setTextAliasEnvironment(text_env)
-        if self.icon is not None:
-            self.icon.setTextAliasEnvironment(text_env)
-
-    def setLocalPosition(self, pos):
-        node = self.movie.getEntityNode()
-        node.setLocalPosition(pos)
-
-    def getLocalPosition(self):
-        node = self.movie.getEntityNode()
-        return node.getLocalPosition()
-
-    def getEntityNode(self):
-        return self.movie.getEntityNode()
-
-    def attachTo(self, node):
-        root = self.movie.getEntityNode()
-        root.removeFromParent()
-        node.addChild(root)
-
-    def getCompositionBounds(self):
-        return self.movie.getCompositionBounds()
-
-    def getSize(self):
-        bounds = self.movie.getCompositionBounds()
-        size = Utils.getBoundingBoxSize(bounds)
-        return size
-
-    def setParam(self, key, value):
-        self.movie.setParam(key, value)
-        if self.icon is not None:
-            self.icon.setParam(key, value)
-
-    def getLayoutSize(self):
-        size = self.getSize()
-        return (size.x, size.y)
-
-    def setLayoutOffset(self, box, offset, size):
-        box_width, box_height = box.getSize()
-        self.setLocalPosition((-box_width/2 + offset[0] + size[0]/2, -box_height/2 + offset[1] + size[1]/2))
